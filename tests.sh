12fc4970048966e52684bf1d9fe50d2b20311a3c
Travis Beatty <travisby@gmail.com>
2018-08-16 07:00:31 -0400

    removed content to fit on 1 page
    Some of the college/tricktech stuff isn't as necessary to show anymore

--- tests.sh
+++ tests.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 pages=$(pdfinfo resume.pdf | grep Pages | awk '{print $2}')
-if [ $pages -gt 2 ]; then
-	echo "Too many pages! ($pages, expecting <= 2)"
+if [ $pages -gt 1 ]; then
+	echo "Too many pages! ($pages, expecting <= 1)"
 	exit 1
 fi
 
